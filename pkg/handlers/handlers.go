6af1a7f2432ee7df1e3da1c4f709769777674b9f
iteplenky <iteplenky@gmail.com>
2024-04-15 15:33:05 +0500

    Initial commit

---
+++ pkg/handlers/handlers.go
@@ -0,0 +1,51 @@
+package handlers
+
+import (
+	"github.com/iteplenky/bookings/pkg/config"
+	"github.com/iteplenky/bookings/pkg/models"
+	"github.com/iteplenky/bookings/pkg/render"
+	"net/http"
+)
+
+// Repo the repository used by the handlers
+var Repo *Repository
+
+// Repository is the repository type
+type Repository struct {
+	App *config.AppConfig
+}
+
+// NewRepo creates a new repository
+func NewRepo(a *config.AppConfig) *Repository {
+	return &Repository{
+		App: a,
+	}
+}
+
+// NewHandlers sets the repository for the handlers
+func NewHandlers(r *Repository) {
+	Repo = r
+}
+
+// Home is the home page handler
+func (m *Repository) Home(w http.ResponseWriter, r *http.Request) {
+	remoteIP := r.RemoteAddr
+	m.App.Session.Put(r.Context(), "remote_ip", remoteIP)
+
+	render.Template(w, "home.page.tmpl", &models.TemplateData{})
+}
+
+// About is the about page handler
+func (m *Repository) About(w http.ResponseWriter, r *http.Request) {
+	// perform some logic
+	stringMap := make(map[string]string)
+	stringMap["test"] = "hello, again"
+
+	remoteIP := m.App.Session.GetString(r.Context(), "remote_ip")
+	stringMap["remote_ip"] = remoteIP
+
+	// send the data to the template
+	render.Template(w, "about.page.tmpl", &models.TemplateData{
+		StringMap: stringMap,
+	})
+}
